1c9cf5c38f0bc843cac71089624205c05ec127d2
masouddaloji <masouddaloji@gmail.com>
2023-03-02 20:22:14 +0330

    add text to persian text

--- frontend/src/text.js
+++ frontend/src/text.js
@@ -8,8 +8,19 @@ export const persianTexts={
         products:{
             addProductsTitle:"افزودن محصول جدید",
             inputLabelTitle:"عنوان محصول",
+            inputLabelPrice:"قیمت محصول",
             inputLabelCover:"کاور اصلی محصول",
-            
+            inputLabelRating:"امتیاز محصول",
+            inputLabelQuantity:"تعداد محصول",
+            inputLabelGallery:"مجموعه عکس های محصول",
+            inputLabelCategory:"دسته بندی محصول",
+            inputLabelSegment:"نام مختصر محصول",
+            inputLabelColors:"رنگ های محصول",
+            inputLabelShortDescription:"توضیحات اجمالی محصول",
+            inputLabelFullDescription:"توضیحات تکمیلی محصول",
+            inputLabelBrand:"برند محصول",
+            inputLabelOffPrice:"درصد تخفیف محصول",
+
         }
     }
 }
