2ccf0230b189ad3c6ef3fc36e48ae792759ad8c1
masouddaloji <masouddaloji@gmail.com>
2023-03-06 15:31:12 +0330

    revert

--- frontend/src/Pages/Register/Register.jsx
+++ frontend/src/Pages/Register/Register.jsx
@@ -1,7 +1,10 @@
-import React, { useState, useRef } from "react";
+import React, { useState,useRef } from "react";
 // library
 import { useLocation } from "react-router-dom";
+<<<<<<< HEAD
 import { Formik, Form } from "formik";
+=======
+>>>>>>> parent of 62a742b (use react formik in project)
 // icons
 import { MdAlternateEmail } from "react-icons/md";
 import { BiHide, BiShow } from "react-icons/bi";
@@ -11,37 +14,37 @@ import { IoPersonOutline } from "react-icons/io5";
 // components
 import Input from "../../components/Input/Input";
 import { persianTexts } from "../../text";
-import Validator from "../../components/Validator/Validator";
 
 // styles
 import "./Register.css";
 
+
 export default function Register() {
-  // const [formState, inputChangeHandler] = useForm(
-  //   {
-  //     phoneNamber: {
-  //       value: "",
-  //       isValid: false,
-  //     },
-  //     userName: {
-  //       value: "",
-  //       isValid: false,
-  //     },
-  //     email: {
-  //       value: "",
-  //       isValid: false,
-  //     },
-  //     password: {
-  //       value: "",
-  //       isValid: false,
-  //     },
-  //     confirmPassword:{
-  //       value:"",
-  //       isValid:false
-  //     },
-  //   },
-  //   false
-  // );
+  const [formState, inputChangeHandler] = useForm(
+    {
+      phoneNamber: {
+        value: "",
+        isValid: false,
+      },
+      userName: {
+        value: "",
+        isValid: false,
+      },
+      email: {
+        value: "",
+        isValid: false,
+      },
+      password: {
+        value: "",
+        isValid: false,
+      },
+      confirmPassword:{
+        value:"",
+        isValid:false
+      },
+    },
+    false
+  );
   return (
     <div className="auth">
       <div className="auth__wrapper">
@@ -55,6 +58,7 @@ export default function Register() {
           </div>
 
           <h4 className="auth__title">ثبت نام</h4>
+<<<<<<< HEAD
           {/* <Formik
             initialValues={{
               registerPhoneNumber: "",
@@ -79,27 +83,61 @@ export default function Register() {
                 name="registerUserName"
                 icon=<IoPersonOutline className="input__icon" />
               />
+=======
+          <form className="auth__form">
+            <Input
+              inputChangeHandler={inputChangeHandler}
+              type="text"
+              label="شماره موبایل"
+              name="phoneNamber"
+              icon=<HiOutlineDevicePhoneMobile className="input__icon" />
+              validation="phoneNamber"
+              errorText={persianTexts.error.register.input.phoneNumber}
+            />
+            <Input
+              inputChangeHandler={inputChangeHandler}
+              type="text"
+              label="نام کاربری"
+              name="userName"
+              icon=<IoPersonOutline className="input__icon" />
+              validation="userName"
+              errorText={persianTexts.error.register.input.userName}
+            />
+>>>>>>> parent of 62a742b (use react formik in project)
 
-              <Input
-                type="email"
-                label="ایمیل"
-                name="registerEmail"
-                icon=<MdAlternateEmail className="input__icon" />
-              />
-              <Input
-                type="text"
-                label="رمز عبور"
-                name="registerPassword"
-                icon=<BiShow className="input__icon" />
-              />
-              <Input
-                type="text"
-                label="تکرار رمز عبور"
-                name="registerConfirmpassword"
-                icon=<BiShow className="input__icon" />
-              />
+            <Input
+              inputChangeHandler={inputChangeHandler}
+              type="email"
+              label="ایمیل"
+              name="email"
+              icon=<MdAlternateEmail className="input__icon" />
+              validation="email"
+              errorText={persianTexts.error.register.input.email}
 
+<<<<<<< HEAD
               <button
+=======
+            />
+            <Input
+              inputChangeHandler={inputChangeHandler}
+              type="text"
+              label="رمز عبور"
+              name="password"
+              icon=<BiShow className="input__icon" />
+              validation="password"
+              errorText={persianTexts.error.register.input.password}
+            />
+            <Input
+              inputChangeHandler={inputChangeHandler}
+              type="text"
+              label="تکرار رمز عبور"
+              name="confirmpassword"
+              icon=<BiShow className="input__icon" />
+              errorText={persianTexts.error.register.input.password}
+            />
+
+            <button
+>>>>>>> parent of 62a742b (use react formik in project)
               disabled={!formState.isFormValid}
               type="submit"
               className={`register__btn ${
@@ -110,8 +148,12 @@ export default function Register() {
             >
               عضویت
             </button>
+<<<<<<< HEAD
             </Form>
           </Formik> */}
+=======
+          </form>
+>>>>>>> parent of 62a742b (use react formik in project)
         </div>
       </div>
     </div>
